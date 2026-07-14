70cfa7a8b0440d216a6b6bdc737ec3459767005b
Abinandhana K V <abinandhanakv699@gmail.com>
2026-07-14 14:11:29 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,7 @@
+// Last updated: 7/14/2026, 2:11:26 PM
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+            return nums;        
+            }
+    }
