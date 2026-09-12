3e8f2b05380d6252fca9403a863add94b5601f73
Abinandhana K V <abinandhanakv699@gmail.com>
2026-09-12 11:34:46 +0530

    Create 1248-CountNumberofNiceSubarrays

---
+++ 1248-CountNumberofNiceSubarrays/version/java/1248-CountNumberofNiceSubarrays_20260912_113445.java
@@ -0,0 +1,18 @@
+// Last updated: 9/12/2026, 11:34:45 AM
+1class Solution {
+2        public int numberOfSubarrays(int[] A, int k) {
+3        int res = 0, i = 0, count = 0, n = A.length;
+4        for (int j = 0; j < n; j++) {
+5            if (A[j] % 2 == 1) {
+6                --k;
+7                count = 0;
+8            }
+9            while (k == 0) {
+10                k += A[i++] & 1;
+11                ++count;
+12            }
+13            res += count;
+14        }
+15        return res;
+16    }
+17}
